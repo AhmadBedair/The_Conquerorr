d8df9198841968c822cb48f5efb3c6052c5ce50f
amir-ammar <75969308+amir-ammar@users.noreply.github.com>
2021-05-31 10:52:54 +0200

    Update Building.java

--- src/buildings/Building.java
+++ src/buildings/Building.java
@@ -54,14 +54,29 @@ abstract public class Building {
 	
 	
 	public void upgrade() throws BuildingInCoolDownException, MaxLevelException{
-		if(this instanceof Farm) {
+		if(this.getLevel()==3) {
+			throw new MaxLevelException("you already reached the max level");
+		}
+		
+		if(this instanceof Farm && this.getLevel()==1) {
+			this.setLevel(2);
+			this.setUpgradeCost(500);
+		}else if (this instanceof Market && this.getLevel()==1) {
 			this.setLevel(2);
 			this.setUpgradeCost(700);
-		}else if (this instanceof Market) {
-			this.setLevel(2);
+		}
+		
+		else if(this instanceof Farm && this.getLevel()==2) {
+			this.setLevel(3);
+			this.setUpgradeCost(700);
+		}
+		else if (this instanceof Market && this.getLevel()==2) {
+			this.setLevel(3);
 			this.setUpgradeCost(1000);
 		}
-		this.setCoolDown(false);
+		
+		
+		
 	}
 	
 	
